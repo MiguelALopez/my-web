a28bf3db09cafef7d08c8cf4c598c27362ff2351
Miguel Lopez <miguel123dr@gmail.com>
2024-06-07 18:06:31 -0500

    Added scroll arrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import "./style.scss"
 export default function Home() {
   const chipClass = 'bg-white/10 rounded-full px-5 py-1.5 text-xs font-normal uppercase tracking-widest'
   return (
@@ -6,10 +6,14 @@ export default function Home() {
       <h1 className="mb-6 text-lg md:text-2xl font-medium">Hello There!</h1>
       <h1 className="text-3xl sm:text-5xl md:text-7xl font-semibold">I'm Miguel Lopez</h1>
       <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-7">
-        <span className={chipClass}>Senior Web Developer</span>
+        <span className={chipClass}>Web Developer</span>
         <span className={chipClass}>FullStack</span>
         <span className={chipClass}>Front End</span>
       </div>
+      <div className="go-to go-to-next"><a href="#about"><span></span></a></div>
+      <svg className="animate-bounce w-6 h-6 ...">
+        <path fill="white" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+      </svg>
     </main>
   );
 }
