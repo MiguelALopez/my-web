998af7be3be1459706a3379174b00efe68161236
Miguel Lopez <miguel123dr@gmail.com>
2024-06-07 10:16:59 -0500

    Fixed styles for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 export default function Home() {
   const chipClass = 'bg-white/10 rounded-full px-5 py-1.5 text-xs font-normal uppercase tracking-widest'
   return (
-    <main className="flex items-center justify-center min-h-screen flex-col p-24 content-center">
+    <main className="flex items-center justify-center min-h-screen flex-col p-12 lg:p-24 content-center">
       <h1 className="mb-6 text-2xl font-medium">Hello There!</h1>
       <h1 className="text-7xl font-semibold">I'm Miguel Lopez</h1>
-      <div className="flex flex-row justify-center items-center gap-4 mt-7">
+      <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-7">
         <span className={chipClass}>Senior Web Developer</span>
         <span className={chipClass}>FullStack</span>
         <span className={chipClass}>Front End</span>
