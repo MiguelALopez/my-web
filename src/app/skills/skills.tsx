251d1325fe4ddb5134f79ff18bea120016fef397
Miguel Lopez <miguel123dr@gmail.com>
2024-06-24 14:33:55 -0500

    Fix over skills section

--- src/app/skills/skills.tsx
+++ src/app/skills/skills.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { FaAngular, FaCss3Alt, FaHtml5, FaReact } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
-import './skills.scss';
 import React, { RefObject, useEffect, useRef, useState } from 'react';
 import { BiLogoTypescript } from 'react-icons/bi';
+import './skills.scss';
 
 export function useOnScreen(ref: RefObject<HTMLElement>) {
 
@@ -36,19 +36,21 @@ export function useOnScreen(ref: RefObject<HTMLElement>) {
 }
 
 export default function Skills() {
+
   const skills = [
-    {name: 'Angular', icon: FaAngular, years: 6,},
-    {name: 'Javascript', icon: IoLogoJavascript, years: 6,},
+    {name: 'HTML5', icon: FaHtml5, years: 8,},
+    {name: 'CSS3', icon: FaCss3Alt, years: 8,},
+    {name: 'Javascript', icon: IoLogoJavascript, years: 8,},
     {name: 'Typescript', icon: BiLogoTypescript, years: 6,},
-    {name: 'HTML5', icon: FaHtml5, years: 6,},
-    {name: 'CSS3', icon: FaCss3Alt, years: 6,},
+    {name: 'Angular', icon: FaAngular, years: 6,},
     {name: 'React', icon: FaReact, years: 1,},
   ];
 
   const ref = useRef<HTMLDivElement>(null);
   const isVisible = useOnScreen(ref);
   const style = (years: number) => ({
-    ['--p']: `${isVisible ? years * 10 : 0}%`
+    ['--p']: `${isVisible ? years * 10 : 10}`,
+    ['--v']: `${isVisible ? 'running' : 'initial'}`,
   }) as React.CSSProperties;
   return (
     <div id={'skills'} className="bg-white py-24 text-zinc-900">
@@ -61,10 +63,18 @@ export default function Skills() {
           {skills.map((skill, index) => (
             <div key={index} className={'flex flex-col justify-center items-center'}>
               <div ref={ref} className={'skill-box'} style={style(skill.years)}>
+                <svg className="svg-circle" viewBox="0 0 40 40">
+                  <g stroke="none" strokeWidth="2" fill="none" fillRule="evenodd">
+                    <circle className="circle" stroke="#111827"
+                            strokeDasharray={`${skill.years * 10} ${100 - skill.years * 10}`}
+                            strokeLinecap="round"
+                            cx="20" cy="20" r="15.91549430918954"></circle>
+                  </g>
+                </svg>
                 <div className={'skill flex justify-center items-center size-16'}>
                   <skill.icon className={'size-8'}/>
                   <div className={'years flex flex-col justify-center items-center'}>
-                    <h1 className={'text-xl font-bold'}>{skill.years}</h1>
+                    <h1>{skill.years}</h1>
                     <p>{skill.years > 1 ? 'Years' : 'Year'}</p>
                   </div>
                 </div>
